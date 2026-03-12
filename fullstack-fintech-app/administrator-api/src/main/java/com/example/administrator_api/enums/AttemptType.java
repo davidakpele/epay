69bbc9213203a5267c9a71d01e82bffd0eb23c15
davidakpele <akpeledavid@hotmail.com>
2026-03-12 15:01:34 +0100

    updating features

--- fullstack-fintech-app/administrator-api/src/main/java/com/example/administrator_api/enums/AttemptType.java
+++
@@ -1,5 +0,0 @@
-package com.example.administrator_api.enums;
-
-public enum AttemptType {
-    LOGIN, WITHDRAWAL, DEPOSIT, EXCESSIVE_FAILED_ATTEMPTS
-}
--- fullstack-fintech-app/administrator-api/src/main/java/com/example/administrator_api/enums/Role.java
+++
@@ -1,7 +0,0 @@
-package com.example.administrator_api.enums;
-
-public enum Role {
-    ADMIN,  // All caps
-    USER,   // All caps
-    MANAGER // All caps 
-}
