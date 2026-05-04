70db389a205a2976fe4392a00b38c9060d891f74
davidakpele <akpeledavid@hotmail.com>
2026-05-04 15:52:51 +0100

    updating features

--- fullstack-fintech-app/resiliences-service/obj/Debug/net9.0/resiliences-service.AssemblyInfo.cs
+++ fullstack-fintech-app/resiliences-service/obj/Debug/net9.0/resiliences-service.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("resiliences-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b1b1331d38b10c17bbdcee978cc13f8c6ac3e246")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e90f2ba7ba120ad858ac7035e84692cbbde3573d")]
 [assembly: System.Reflection.AssemblyProductAttribute("resiliences-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("resiliences-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
